cd2587b91e4420b084d36223a2454b0de2bcc1ed
David Valverde <val25002@byui.edu>
2026-04-24 20:02:31 -0600

    1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,9 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) //always start with main//
     {
+        //how you are meant to print//
         Console.WriteLine("Hello Prep1 World!");
     }
 }
