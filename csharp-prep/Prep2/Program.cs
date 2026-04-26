3780a5308d0f30c0f03c6b65afc62e08dfe25c6d
David Valverde <val25002@byui.edu>
2026-04-25 23:39:41 -0600

    grade assignment done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string dav_percent = Console.ReadLine();
+        int dav_percentint = int.Parse(dav_percent);
+
+        string dav_grade = "";
+
+
+        if (dav_percentint >= 90)
+        {
+            dav_grade = "A";
+        }
+        else if (dav_percentint >= 80)
+        {
+            dav_grade = "B";
+        }
+        else if (dav_percentint >= 70)
+        {
+            dav_grade = "C";
+        }
+        else if (dav_percentint >= 60)
+        {
+            dav_grade = "D";
+        }
+        else
+        {
+            dav_grade = "F";
+        }
+
+        Console.WriteLine($"Grade:{dav_grade}");
+        
+        if (dav_percentint >= 70)
+        {
+            Console.WriteLine("Pass");
+        }
+        else
+        {
+            Console.WriteLine("Fail");
+        }
     }
 }
