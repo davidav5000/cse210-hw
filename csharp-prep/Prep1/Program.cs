46b7c9ad94d81117551fd5df7db372b787a53b8b
David Valverde <val25002@byui.edu>
2026-04-25 23:27:55 -0600

    james bond fin

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,6 +7,12 @@ class Program
         //how you are meant to print//
         Console.Write("What is you first name? ");
         string dav_firstname = Console.ReadLine();
+
+        Console.Write("What is you last name? ");
+        string dav_lastname = Console.ReadLine();
+
+        Console.WriteLine($"You name is {dav_lastname}, {dav_firstname} {dav_lastname}.");
+
     }
 }
 
