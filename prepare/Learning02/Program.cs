13344e833d46282cd63fcd82de6106179f6900d6
David Valverde <val25002@byui.edu>
2026-05-05 18:44:13 -0600

    progress on Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Executive";
+        job2._company = "Apple";
     }
 }
